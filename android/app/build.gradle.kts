ba602c2d888a1f43ec019d2b9ddb085d21666703
alberto <albertorolibert@gmail.com>
2025-11-19 19:50:47 -0500

    Añadidos los archivos .aab .apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
   id("com.android.application")
   id("kotlin-android")
@@ -6,6 +9,13 @@ plugins {
   id("com.google.gms.google-services")
 
 }
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 
 dependencies {
   // Import the Firebase BoM
@@ -48,11 +58,20 @@ android {
         setProperty("archivesBaseName", "Juanchos")
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
